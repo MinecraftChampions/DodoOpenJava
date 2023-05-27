d1c88b7a8eefc3ddea06452a925513c28cd86355
qscbm187531 <1309635304@qq.com>
2023-05-27 20:26:15 +0800

    增加保留注释的Yaml实例，改进Maven版本号管理

---
+++ configuration/src/main/java/io/github/minecraftchampions/dodoopenjava/configuration/CommentConfiguration.java
@@ -0,0 +1,94 @@
+package io.github.minecraftchampions.dodoopenjava.configuration;
+
+import io.github.minecraftchampions.dodoopenjava.configuration.file.YamlConfiguration;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * 保存时保留配置文件注释的Yaml实例
+ * 会以KV的形式存储
+ * ”'comment   # 注释内容': comment“的格式存储
+ */
+public class CommentConfiguration extends YamlConfiguration {
+    protected static String commentPrefixSymbol = "'comment ";
+    protected static String commentSuffixSymbol = "': comment";
+    protected static String fromRegex = "( *)(#.*)";
+    protected static Pattern fromPattern = Pattern.compile(fromRegex);
+    protected static String toRegex = "( *)(- )*" + "(" + commentPrefixSymbol + ")" + "(#.*)" + "(" + commentSuffixSymbol + ")";
+    protected static Pattern toPattern = Pattern.compile(toRegex);
+    protected static Pattern countSpacePattern = Pattern.compile("( *)(- )*(.*)");
+    protected static int commentSplitWidth = 90;
+    private static String[] split(String string, int partLength) {
+        String[] array = new String[string.length() / partLength + 1];
+        for (int i = 0; i < array.length; i++) {
+            int beginIndex = i * partLength;
+            int endIndex = beginIndex + partLength;
+            if (endIndex > string.length()) {
+                endIndex = string.length();
+            }
+            array[i] = string.substring(beginIndex, endIndex);
+        }
+        return array;
+    }
+
+    @Override
+    public void loadFromString(String contents) throws InvalidConfigurationException {
+        String[] parts = contents.split("\n");
+        List<String> lastComments = new ArrayList<>();
+        StringBuilder builder = new StringBuilder();
+        for (String part : parts) {
+            Matcher matcher = fromPattern.matcher(part);
+            if (matcher.find()) {
+                String originComment = matcher.group(2);
+                String[] splitComments = split(originComment, commentSplitWidth);
+                for (int i = 0; i < splitComments.length; i++) {
+                    String comment = splitComments[i];
+                    if (i == 0) {
+                        comment = comment.substring(1);
+                    }
+                    comment = COMMENT_PREFIX + comment;
+                    lastComments.add(comment.replaceAll("\\.", "．").replaceAll("'", "＇").replaceAll(":", "："));
+                }
+            } else {
+                matcher = countSpacePattern.matcher(part);
+                if (matcher.find() && !lastComments.isEmpty()) {
+                    for (String comment : lastComments) {
+                        builder.append(matcher.group(1));
+                        builder.append(this.checkNull(matcher.group(2)));
+                        builder.append(commentPrefixSymbol);
+                        builder.append(comment);
+                        builder.append(commentSuffixSymbol);
+                        builder.append("\n");
+                    }
+                    lastComments.clear();
+                }
+                builder.append(part);
+                builder.append("\n");
+            }
+        }
+        super.loadFromString(builder.toString());
+    }
+
+    @Override
+    public String saveToString() {
+        String contents = super.saveToString();
+        StringBuilder savcontent = new StringBuilder();
+        String[] parts = contents.split("\n");
+        for (String part : parts) {
+            Matcher matcher = toPattern.matcher(part);
+            if (matcher.find() && matcher.groupCount() == 5) {
+                part = this.checkNull(matcher.group(1)) + matcher.group(4);
+            }
+            savcontent.append(part.replaceAll("．", ".").replaceAll("＇", "'").replaceAll("：", ":"));
+            savcontent.append("\n");
+        }
+        return savcontent.toString();
+    }
+    
+    private String checkNull(String string) {
+        return string == null ? "" : string;
+    }
+}
