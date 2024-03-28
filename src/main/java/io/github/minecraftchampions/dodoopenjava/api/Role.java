d34a15c57f0540635162fae7bf300a419a78adae
qscbm187531 <1309635304@qq.com>
2024-03-28 19:13:42 +0800

    Update Role.java

--- src/main/java/io/github/minecraftchampions/dodoopenjava/api/Role.java
+++ src/main/java/io/github/minecraftchampions/dodoopenjava/api/Role.java
@@ -1,7 +1,27 @@
 package io.github.minecraftchampions.dodoopenjava.api;
 
+import io.github.minecraftchampions.dodoopenjava.Result;
+import io.github.minecraftchampions.dodoopenjava.permission.Permission;
+
 /**
  * 身份组
  */
 public interface Role {
+    String getRoleId();
+
+    Result create();
+
+    Result delete();
+
+    long getMemberCount();
+
+    Result editRoleName(String name);
+
+    Result editRoleColor(String color);
+
+    Result editPosition(int position);
+
+    Result editPermission(Permission permission);
+
+    Result editPermission(String permission);
 }
