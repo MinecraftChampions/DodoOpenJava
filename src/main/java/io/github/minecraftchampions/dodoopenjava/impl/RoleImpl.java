6e6ab94b35ec1f5d49c3ccfe1c079330cf700271
qscbm187531 <1309635304@qq.com>
2024-04-04 20:10:50 +0800

    Create RoleImpl.java

---
+++ src/main/java/io/github/minecraftchampions/dodoopenjava/impl/RoleImpl.java
@@ -0,0 +1,70 @@
+package io.github.minecraftchampions.dodoopenjava.impl;
+
+import io.github.minecraftchampions.dodoopenjava.Result;
+import io.github.minecraftchampions.dodoopenjava.api.Role;
+import io.github.minecraftchampions.dodoopenjava.api.User;
+import io.github.minecraftchampions.dodoopenjava.permission.Permission;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NonNull;
+
+import java.util.List;
+
+/**
+ * 身份组实现
+ */
+@Getter
+@AllArgsConstructor
+public class RoleImpl implements Role {
+    @NonNull
+    private String roleId;
+    @Override
+    public Result delete() {
+        return null;
+    }
+
+    @Override
+    public long getMemberCount() {
+        return 0;
+    }
+
+    @Override
+    public Result editRoleName(String name) {
+        return null;
+    }
+
+    @Override
+    public Result editRoleColor(String color) {
+        return null;
+    }
+
+    @Override
+    public Result editPosition(int position) {
+        return null;
+    }
+
+    @Override
+    public Result editPermission(Permission permission) {
+        return null;
+    }
+
+    @Override
+    public Result editPermission(String permission) {
+        return null;
+    }
+
+    @Override
+    public Result removeMember(String dodoId) {
+        return null;
+    }
+
+    @Override
+    public Result addMember(String dodoId) {
+        return null;
+    }
+
+    @Override
+    public List<User> getMemberList() {
+        return null;
+    }
+}
