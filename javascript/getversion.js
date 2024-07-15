50e80d63df2a755b8d9cba2a05782f6de9d1fbdc
mcchampions <1309635304@qq.com>
2024-07-15 22:14:41 +0800

    Create getversion.js

---
+++ javascript/getversion.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+
+const xml2js = require('xml2js');
+
+const parser = new xml2js.Parser(undefined);
+const xml = fs.readFileSync('pom.xml', 'utf8');
+parser.parseString(xml, (err, result) => {
+    if (err) {
+        console.error(err);
+        return;
+    }
+    const version = result.project.version[0];
+    console.info("DodoOpenJavaVersion=" + version)
+    fs.writeFileSync(process.env.GITHUB_OUTPUT, 'version=' + version);
+});
