05533be927ce001273a7cb418726822362d21892
mcchampions <1309635304@qq.com>
2024-07-15 21:53:42 +0800

    Update getversion.js

--- javascript/getversion.js
+++ javascript/getversion.js
@@ -1,8 +1,10 @@
 const fs = require('fs');
 
-var xmldoc=document.implementation.createDocument('','',null);
-xmldoc.async=false;
-xmldoc.load('../pom.xml');
-var version = xmldoc.getElementsByName("version")[0];
+let convert = require('xml-js');
+
+let xml = fs.readFileSync("../pom.xml",'utf-8');
+let result = convert.xml2json(xml, {compact: true, spaces: 4});
+const json = JSON.parse(result);
+const version = json.project.version;
 console.info("DodoOpenJavaVersion=" + version.textContent)
 fs.writeFileSync(process.env.GITHUB_OUTPUT, 'version=' + version.textContent);
