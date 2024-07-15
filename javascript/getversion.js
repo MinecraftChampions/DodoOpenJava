7d1aa0c4bed71fa81bf5c5e3b23ae7deafef339c
mcchampions <1309635304@qq.com>
2024-07-15 22:12:42 +0800

    Update getversion.js

--- javascript/getversion.js
+++ javascript/getversion.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const xml2js = require('xml2js');
 
 const parser = new xml2js.Parser(undefined);
-const xml = fs.readFileSync('../pom.xml', 'utf8');
+const xml = fs.readFileSync('pom.xml', 'utf8');
 parser.parseString(xml, (err, result) => {
     if (err) {
         console.error(err);
