e29ae11942ce4a6d9990e708b8003fc9348d81e9
mcchampions <1309635304@qq.com>
2024-12-13 12:50:06 +0800

    Update NetUtils.java

--- src/main/java/io/github/minecraftchampions/dodoopenjava/utils/NetUtils.java
+++ src/main/java/io/github/minecraftchampions/dodoopenjava/utils/NetUtils.java
@@ -221,7 +221,7 @@ public class NetUtils {
         }
     }
 
-    private static Map<String, String> browserHeaderMap = new HashMap<>(
+    private static final Map<String, String> browserHeaderMap = new HashMap<>(
             Map.of("User-Agent",
                     "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36"));
 
